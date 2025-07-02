3558c97feed5b24fb69d758d3e49c71e9a884ecd
HarshaVardhan <harsavardhan108@gmail.com>
2025-07-02 18:48:04 +0530

    Add portfolio/tailwind.config.js 1751462283999

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'light-pink': '#FFEDF3',
+        'mint-green': '#ADEED9',
+        'teal': '#56DFCF',
+        'dark-teal': '#0ABAB5',
+      },
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
